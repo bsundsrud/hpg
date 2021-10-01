ee32cbd9589d9594f4d79055c187434e5de8610c
Benn Sundsrud <benn.sundsrud@gmail.com>
2021-09-30 23:51:43 -0500

    Add structopt for opt parsing, calculate execution order for tasks given on command line

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,24 @@
-use std::collections::HashMap;
+use std::{
+    collections::{HashMap, HashSet, VecDeque},
+    fmt::Display,
+};
 
+use anyhow::Result;
 use petgraph::prelude::*;
 use rlua::{self, Lua};
+use structopt::StructOpt;
+use thiserror::Error;
 
 pub type TaskIdx = NodeIndex<u32>;
 pub type TaskGraph = DiGraph<TaskDefinition, (), u32>;
+
+#[derive(Debug, Error)]
+pub enum TaskError {
+    #[error("Cycle detected involving {0}")]
+    CycleError(TaskRef),
+    #[error("Unknown task {0}")]
+    UnknownTask(TaskRef),
+}
 
 #[derive(Debug, PartialEq, Eq, Hash, Clone)]
 pub struct TaskRef(String);
@@ -27,6 +41,12 @@ impl From<&str> for TaskRef {
     }
 }
 
+impl Display for TaskRef {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(&self.0)
+    }
+}
+
 #[derive(Debug)]
 pub struct TaskDefinition {
     name: TaskRef,
@@ -42,34 +62,88 @@ impl TaskDefinition {
     }
 }
 
-fn create_graph(tasks: Vec<TaskDefinition>) -> TaskGraph {
-    let mut dag = TaskGraph::new();
-    let mut idx_map: HashMap<TaskRef, TaskIdx> = HashMap::new();
+pub struct TaskGraphState {
+    dag: TaskGraph,
+    ref_to_nodes: HashMap<TaskRef, TaskIdx>,
+}
 
-    for task in tasks {
-        let r = task.name.clone();
-        let idx = dag.add_node(task);
-        idx_map.insert(r, idx);
-    }
-    let mut dep_map: HashMap<TaskIdx, Vec<TaskIdx>> = HashMap::new();
+impl TaskGraphState {
+    pub fn from_tasks(tasks: Vec<TaskDefinition>) -> Result<Self> {
+        let mut dag = TaskGraph::new();
+        let mut idx_map: HashMap<TaskRef, TaskIdx> = HashMap::new();
 
-    for n in dag.node_weights() {
-        let idx = idx_map.get(&n.name).unwrap();
-        for dep_ref in n.dependencies.iter() {
-            let dep_idx = idx_map.get(dep_ref).unwrap();
-            dep_map
-                .entry(*idx)
-                .or_insert_with(|| Vec::new())
-                .push(*dep_idx);
+        for task in tasks {
+            let r = task.name.clone();
+            let idx = dag.add_node(task);
+            idx_map.insert(r, idx);
         }
+        let mut dep_map: HashMap<TaskIdx, Vec<TaskIdx>> = HashMap::new();
+
+        for n in dag.node_weights() {
+            let idx = idx_map
+                .get(&n.name)
+                .ok_or_else(|| TaskError::UnknownTask(n.name.clone()))?;
+            for dep_ref in n.dependencies.iter() {
+                let dep_idx = idx_map
+                    .get(dep_ref)
+                    .ok_or_else(|| TaskError::UnknownTask(dep_ref.clone()))?;
+                dep_map
+                    .entry(*idx)
+                    .or_insert_with(|| Vec::new())
+                    .push(*dep_idx);
+            }
+        }
+
+        for (idx, v) in dep_map.iter() {
+            for dep_idx in v {
+                dag.add_edge(*idx, *dep_idx, ());
+            }
+        }
+        Ok(Self {
+            dag,
+            ref_to_nodes: idx_map,
+        })
     }
 
-    for (idx, v) in dep_map.iter() {
-        for dep_idx in v {
-            dag.add_edge(*dep_idx, *idx, ());
+    fn dfs_post(&self, task: TaskIdx) -> Vec<TaskIdx> {
+        let mut dfs = DfsPostOrder::new(&self.dag, task);
+        let mut res = Vec::new();
+        while let Some(nx) = dfs.next(&self.dag) {
+            res.push(nx);
         }
+        res
     }
-    dag
+
+    pub fn execution_for_task(&self, task: &TaskRef) -> Result<Vec<&TaskDefinition>> {
+        let start_idx = self
+            .ref_to_nodes
+            .get(task)
+            .ok_or_else(|| TaskError::UnknownTask(task.clone()))?;
+
+        Ok(self
+            .dfs_post(*start_idx)
+            .into_iter()
+            .map(|idx| self.dag.node_weight(idx).unwrap())
+            .collect())
+    }
+
+    pub fn execution_for_tasks(&self, tasks: Vec<TaskRef>) -> Result<Vec<&TaskDefinition>> {
+        let mut uniques = HashSet::new();
+
+        let mut execution: Vec<&TaskDefinition> = tasks
+            .into_iter()
+            .map(|task| self.execution_for_task(&task)) // Results in Vec<Result<Vec<_>>>
+            .collect::<Result<Vec<_>>>()? // Transform to Result<Vec<Vec<_>>> and handle errors here
+            .into_iter()
+            .flatten() // Flatten the double-Vec and merge all execution chains
+            .collect();
+
+        // Execution is in topo-order for the given tree(s), if there's any overlap
+        // between the given tasks we retain the entry that is higher in the execution order
+        execution.retain(|e| uniques.insert(e.name.clone()));
+
+        Ok(execution)
+    }
 }
 
 fn create_lua() -> Lua {
@@ -86,10 +160,19 @@ fn create_lua() -> Lua {
     lua
 }
 
-fn main() {
+#[derive(Debug, StructOpt)]
+#[structopt(name = "hpg", about = "config management tool")]
+struct Opt {
+    #[structopt(name = "TARGETS")]
+    targets: Vec<String>,
+}
+
+fn main() -> Result<()> {
+    let opt = Opt::from_args();
+
     let tasks = vec![
         TaskDefinition::new("A", Vec::new()),
-        TaskDefinition::new("B", vec!["A"]),
+        TaskDefinition::new("B", vec!["A", "F"]),
         TaskDefinition::new("C", vec!["A"]),
         TaskDefinition::new("D", vec!["B"]),
         TaskDefinition::new("E", vec!["D", "C"]),
@@ -98,10 +181,12 @@ fn main() {
         TaskDefinition::new("H", vec!["G"]),
     ];
 
-    let dag = create_graph(tasks);
-    let res = petgraph::algo::toposort(&dag, None).unwrap();
-    for idx in res {
-        let task = &dag[idx];
-        println!("{:?}", task.name);
+    let state = TaskGraphState::from_tasks(tasks)?;
+
+    let task_refs = opt.targets.into_iter().map(TaskRef::new).collect();
+    let ordering = state.execution_for_tasks(task_refs)?;
+    for task in ordering {
+        println!("{}", task.name);
     }
+    Ok(())
 }
