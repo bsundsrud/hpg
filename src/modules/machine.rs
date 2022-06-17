37412b5322a967b697116de623648ae596e52887
Benn Sundsrud <benn.sundsrud@gmail.com>
2022-06-16 22:25:47 -0500

    Update dependency versions

--- src/modules/machine.rs
+++ src/modules/machine.rs
@@ -8,12 +8,13 @@ pub fn machine(lua: &Lua) -> Result<()> {
         let tbl = lua_ctx.create_table()?;
         let uname_tbl = lua_ctx.create_table()?;
 
-        let uname_info = uname();
-        uname_tbl.set("sysname", uname_info.sysname())?;
-        uname_tbl.set("nodename", uname_info.nodename())?;
-        uname_tbl.set("release", uname_info.release())?;
-        uname_tbl.set("version", uname_info.version())?;
-        uname_tbl.set("machine", uname_info.machine())?;
+        let uname_info = uname()
+            .map_err(|e| TaskError::ActionError(format!("Unable to run uname, err: {}", e)))?;
+        uname_tbl.set("sysname", uname_info.sysname().to_string_lossy().as_ref())?;
+        uname_tbl.set("nodename", uname_info.nodename().to_string_lossy().as_ref())?;
+        uname_tbl.set("release", uname_info.release().to_string_lossy().as_ref())?;
+        uname_tbl.set("version", uname_info.version().to_string_lossy().as_ref())?;
+        uname_tbl.set("machine", uname_info.machine().to_string_lossy().as_ref())?;
         tbl.set("uname", uname_tbl)?;
         lua_ctx.globals().set("machine", tbl)?;
         Ok(())
